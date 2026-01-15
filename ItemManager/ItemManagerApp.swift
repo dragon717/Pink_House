61280a41d56fca014d2d79e4e04c965dbb17c30c
muniao <825188288@qq.com>
2026-01-15 22:55:52 +0800

    Initial Commit

---
+++ ItemManager/ItemManagerApp.swift
@@ -0,0 +1,32 @@
+//
+//  ItemManagerApp.swift
+//  ItemManager
+//
+//  Created by 木鸟 on 1/15/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ItemManagerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
