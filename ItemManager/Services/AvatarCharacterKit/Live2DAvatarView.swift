5c6a74d885c51b4572118359768ce72f069c8724
dragon717 <825188288@qq.com>
2026-05-01 13:08:40 +0800

    [A5] live2d native spike

--- ItemManager/Services/AvatarCharacterKit/Live2DAvatarView.swift
+++ ItemManager/Services/AvatarCharacterKit/Live2DAvatarView.swift
@@ -1,5 +1,6 @@
 import MetalKit
 import SwiftUI
+import UIKit
 
 struct Live2DAvatarView: UIViewRepresentable {
     let request: AvatarRenderRequest
@@ -15,22 +16,69 @@ struct Live2DAvatarView: UIViewRepresentable {
         view.clearColor = MTLClearColor(red: 0, green: 0, blue: 0, alpha: 0)
         view.framebufferOnly = true
         view.enableSetNeedsDisplay = false
-        view.isPaused = request.isPaused
         view.delegate = context.coordinator
+        context.coordinator.attach(view: view)
         context.coordinator.configure(request: request)
         return view
     }
 
     func updateUIView(_ uiView: MTKView, context: Context) {
-        uiView.isPaused = request.isPaused
+        context.coordinator.attach(view: uiView)
         context.coordinator.configure(request: request)
     }
 
     final class Coordinator: NSObject, MTKViewDelegate {
         private var request = AvatarRenderRequest()
+        private weak var view: MTKView?
+        private var isSystemPaused = false
+        private var backgroundObserver: NSObjectProtocol?
+        private var foregroundObserver: NSObjectProtocol?
+
+        override init() {
+            super.init()
+            let center = NotificationCenter.default
+            backgroundObserver = center.addObserver(
+                forName: UIApplication.didEnterBackgroundNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                self?.setSystemPaused(true)
+            }
+            foregroundObserver = center.addObserver(
+                forName: UIApplication.willEnterForegroundNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] _ in
+                self?.setSystemPaused(false)
+            }
+        }
+
+        deinit {
+            if let backgroundObserver {
+                NotificationCenter.default.removeObserver(backgroundObserver)
+            }
+            if let foregroundObserver {
+                NotificationCenter.default.removeObserver(foregroundObserver)
+            }
+        }
+
+        func attach(view: MTKView) {
+            self.view = view
+            applyPauseState()
+        }
 
         func configure(request: AvatarRenderRequest) {
             self.request = request
+            applyPauseState()
+        }
+
+        private func setSystemPaused(_ isPaused: Bool) {
+            isSystemPaused = isPaused
+            applyPauseState()
+        }
+
+        private func applyPauseState() {
+            view?.isPaused = request.isPaused || isSystemPaused
         }
 
         func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
